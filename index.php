70d00ade9e3a63fbc4c23f493b001c3d8ae98ef0
DeEpRaJ <99066501+DeViL0502@users.noreply.github.com>
2022-10-09 15:03:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" >
-
 <head>
   <meta charset="UTF-8">
-  <title>loginAPI</title>
-  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Login API</title>
+  
+  
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
+      <link rel="stylesheet" href="./assets/css/style.css">
+  
 </head>
-
 <body>
-
   <div class="login-wrap">
   <div class="login-html">
     <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
     <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
     <div class="login-form">
-      <form class="sign-up" action="./api/user/login.php" method="GET">
+      <form class="sign-in-htm" action="./api/user/login.php" method="GET">
         <div class="group">
           <label for="user" class="label">Username</label>
           <input id="username" name="username" type="text" class="input">
@@ -31,6 +32,9 @@
           <input type="submit" class="button" value="Sign In">
         </div>
         <div class="hr"></div>
+        <div class="foot-lnk">
+          <a href="#forgot">Forgot Password?</a>
+        </div>
       </form>
       <form class="sign-up-htm" action="./api/user/signup.php" method="POST">
         <div class="group">
@@ -58,7 +62,5 @@
 </div>
   
   
-
 </body>
-
 </html>
